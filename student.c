78b13c7f04d50bcc9a8516a168628e00826f3571
Alicja Siudak <alicjask17@gmail.com>
2019-12-03 13:23:40 +0100

    clion to xcode

---
+++ student.c
@@ -0,0 +1,4 @@
+//
+// Created by Alicja Siudak on 03/12/2019.
+//
+
---
+++ student.h
@@ -0,0 +1,8 @@
+//
+// Created by Alicja Siudak on 03/12/2019.
+//
+
+#ifndef ESW_ASSIGNMENT_STUDENT_H
+#define ESW_ASSIGNMENT_STUDENT_H
+
+#endif //ESW_ASSIGNMENT_STUDENT_H
