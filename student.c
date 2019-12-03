f557295e7f1a3a825839932c4ba7960ddf811dd2
Alicja Siudak <alicjask17@gmail.com>
2019-12-03 13:32:25 +0100

    student done?

--- student.c
+++ student.c
@@ -1,4 +1,44 @@
 //
 // Created by Alicja Siudak on 03/12/2019.
 //
+#include <string.h>
+#include "student.h"
+#include <stdio.h>
+
+student_t* createStudent(int student_no, char* f_name){
+    student_t* student = (student_t*)malloc(sizeof(student_t));
+    if(student == NULL){
+        return NULL;
+    }
+    else{
+        student->student_no=student_no;
+        student->f_name = strdup(f_name);
+        return student;
+    }
+}
+
+void destroyStudent(student_t* student){
+
+    free(student->f_name);
+    //make sure nobody will use it
+    char* f_name == NULL;
+    free(student);
+}
+
+void setStudentNo(student_t* student, int student_no){
+    student->student_no=student_no;
+}
+
+int getStudentNo(student_t* student){
+    return student->student_no;
+}
+
+char* getStudentFName(student_t* student){
+    return student->f_name;
+}
+
+void print_Student_Information(student_t *student)
+{
+    printf("S \t %s\t %d\n", student->first_name, student->student_number);
+}
 
--- student.h
+++ student.h
@@ -5,4 +5,19 @@
 #ifndef ESW_ASSIGNMENT_STUDENT_H
 #define ESW_ASSIGNMENT_STUDENT_H
 
+typedef struct student_t {
+    int student_no;
+    char *f_name;
+}student_t;
+
+student_t* createStudent(int student_no, char *f_name);
+void destroyStudent(student_t* student);
+void setStudentNo(student_t* student, int student_no);
+void setStudentFName(student_t* student, char *f_name);
+int getStudentNo(student_t* student);
+char* getStudentFName(student_t* student);
+void print_Student_Information(student_t *student);
+
 #endif //ESW_ASSIGNMENT_STUDENT_H
+
+
